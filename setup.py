50802946e413ed206f746ed3318f3c1ead7b49ac
Nick Riley <nick@sparkida.com>
2014-01-08 15:04:38 -0800

    distro-1

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from distutils.core import setup
+import sys, os
 setup(name='codebake',
 		version='1.0',
 		description='Utility for quickly minifying and obfuscating JavaScript',
