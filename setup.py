75807a394f1aba531be4e647e9a06154b8e2a287
Nicholas Riley <nriley.aytch@gmail.com>
2014-06-09 07:42:23 -0700

    coding utf-8

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 Codebake
 Clean CSS, HTML, and JavaScript Files
@@ -9,28 +10,28 @@ Author: Nicholas Riley
 from os import path
 from setuptools import setup
 from sys import platform
-require = ['argparse','jinja2']
+require = ['argparse']
 if platform.startswith('linux'):
-	require.append('readline')
+    require.append('readline')
 else:
-	require.append('pyreadline')
+    require.append('pyreadline')
 
 
 def read(fname):
-	return open(path.join(path.dirname(__file__), fname)).read()
+    return open(path.join(path.dirname(__file__), fname)).read()
 
 #from sys import platforms
 setup(name='Codebake',
-		version='1.3.2',
-		description='Utility for quickly minifying and obfuscating JavaScript',
-		url='https://github.com/sparkida/codebake',
-		author='Nicholas Riley',
-		author_email='nick@sparkida.com',
-		license='MIT',
-		long_description=read('README.md'),
-		entry_points={'console_scripts': ['bake = Codebake.oven:Codebake']},
-		install_requires=require,
-		py_modules=['bake'],
-		packages=['Codebake'],
-		package_dir={'Codebake':'codebake'})
+        version='1.3.3',
+        description='Utility for quickly minifying and obfuscating JavaScript and CSS',
+        url='https://github.com/sparkida/codebake',
+        author='Nicholas Riley',
+        author_email='nick@sparkida.com',
+        license='MIT',
+        long_description=read('README.md'),
+        entry_points={'console_scripts': ['bake = Codebake.oven:Codebake']},
+        install_requires=require,
+        py_modules=['bake'],
+        packages=['Codebake'],
+        package_dir={'Codebake':'codebake'})
 
